15f2f9dc35bf273d635af532ff5188ae72727ffd
SATURNO\Asus <jlt022@hotmail.com>
2017-03-08 21:35:20 -0500

    First commit

---
+++ SitioFundacionOscarez/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SitioFundacionOscarez.Startup))]
+namespace SitioFundacionOscarez
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
